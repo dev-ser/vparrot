0965302399894db89e5cf1184e3241001c242c69
dev-ser <serap_dev@icloud.com>
2024-02-20 16:20:07 +0100

    Synchronisation avec le projet original

--- public/index.php
+++ public/index.php
@@ -5,5 +5,9 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
+    // Définir les valeurs pour APP_ENV et APP_DEBUG dans le tableau $context
+    $context['APP_ENV'] = 'prod';
+    $context['APP_DEBUG'] = true;
+
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
